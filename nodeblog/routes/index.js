9b63b1838576e3d2f0da4f6b5054c388b5c17df8
Mijea Mihai <mmihai@suitsupply.com>
2016-02-20 21:12:36 +0100

    read blog posts from mongoDB

--- nodeblog/routes/index.js
+++ nodeblog/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+const express = require('express');
+const router = express.Router();
+const mongo = require('mongodb');
+const db = require('monk')('localhost/nodeblog');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* home page blog posts . */
+router.get('/', (req, res, next) => {
+	let db = req.db;
+	let posts = db.get('posts');
+	posts.find({},{}, (err, posts) => {
+		res.render('index',{
+			posts
+		});
+	});
 });
 
 module.exports = router;
